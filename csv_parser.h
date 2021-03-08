2c69b80e09dbd89530cc1f3ed8ce5c1031778577
onkeshonkar <onkeshonkar>
2021-03-09 03:26:58 +0530

    Complete function definations of CSVParser class.

--- csv_parser.h
+++ csv_parser.h
@@ -93,3 +93,402 @@ public:
 /* 
     Funtion defination goes here
 */
+
+void CSVParser::get_validators(unordered_map<string, string (*)()> &validators)
+{
+    if (!validators.size())
+    {
+        cout << "Error: Validators map can't be empty!!!" << endl;
+        exit(1);
+    }
+    if (columns_name.size() <= 1)
+    {
+        cout << "Error: Columns name is empty!!!" << endl;
+        exit(1);
+    }
+
+    // check and intialize if validators exists for each column except UID.
+    for (auto &column : columns_name)
+    {
+        if (column == "UID")
+        {
+            continue;
+        }
+        if (!validators[column])
+        {
+            cout << " Error: No validator function mapped for coumn \'" << column << "\'!!!" << endl;
+            exit(1);
+        }
+        input_validators[column] = validators[column];
+    }
+    validators_configured = true;
+}
+
+void CSVParser::set_file_name(string file_name)
+{
+
+    int dot_pos = file_name.find('.'); // finds index of (.)
+    string extension = file_name.substr(dot_pos + 1);
+    extension = to_lower(extension);
+    if (extension != "csv")
+    {
+        cout << "Error: File with csv extension required!!!" << endl;
+        exit(1);
+    }
+
+    fstream file;
+    file.open(file_name);
+    if (!file.good())
+    {
+        cout << "Error: Cann't find file '" << file_name << "' !!!" << endl;
+        file.close();
+        exit(1);
+    }
+    file.close();
+    this->file_name = file_name;
+}
+
+vector<string> CSVParser::split_at_comma(string &line)
+{
+    vector<string> splited;
+    string word;
+    stringstream ss(line);
+    string new_uid = to_string(prev_uid + 1);
+    splited.push_back(new_uid);
+    while (getline(ss, word, ','))
+    {
+        splited.push_back(word);
+    }
+    return splited;
+}
+
+void CSVParser::set_column_width(vector<string> &row)
+{
+    int max_col_width = 30;
+    for (int i = 0; i < row.size(); i++)
+    {
+        int len = row[i].length() + 2;
+        if (len > max_col_width)
+        {
+            len = 35;
+        }
+        if (columns_width[i] < len)
+        {
+            columns_width[i] = len;
+        }
+    }
+}
+
+void CSVParser::print_columns_name()
+{
+    int total_width = 0;
+
+    for (int i = 0; i < columns_name.size(); ++i)
+    {
+        cout << left << setw(columns_width[i]) << to_upper(columns_name[i]);
+        total_width += columns_width[i];
+    }
+    cout << endl;
+    while (total_width--)
+    {
+        cout << "=";
+    }
+    cout << endl;
+}
+
+void CSVParser::print_row(vector<string> &row)
+{
+
+    for (int i = 0; i < row.size(); ++i)
+    {
+        string word = row[i];
+        if (word.length() > 30)
+        {
+            word = word.substr(0, 30) + "...";
+        }
+        cout << left << setw(columns_width[i]) << word;
+    }
+
+    cout << endl;
+}
+
+void CSVParser::parse_file()
+{
+    fstream file;
+    string line;
+    file.open(file_name);
+    bool first_row = true;
+    if (!file.good())
+    {
+        file.close();
+        cout << "Error: Cann't read!!!" << file_name << endl;
+        exit(1);
+    }
+
+    if (file.peek() == EOF)
+    {
+        return; // function returns if file is empty!
+    }
+
+    cout << "parsing csv file....." << endl;
+    while (getline(file, line))
+    {
+        if (first_row && !use_new_column)
+        {
+            columns_name = split_at_comma(line);
+            columns_name[0] = "UID";
+            first_row = false;
+            columns_width = vector<int>(columns_name.size(), 7);
+        }
+        else
+        {
+            vector<string> splited = split_at_comma(line);
+            parsed_data.push_back(splited);
+            set_column_width(splited);
+            ++prev_uid;
+        }
+    }
+    file.close();
+    cout << "parsing complete." << endl;
+}
+
+void CSVParser::print_all()
+{
+
+    if (!parsed_data.size())
+    {
+        cout << "Warning: nothing to print, parsed data is empty!!!" << endl;
+        return;
+    }
+
+    print_columns_name();
+    int total_row = 0;
+    for (auto &row : parsed_data)
+    {
+        print_row(row);
+        ++total_row;
+    }
+
+    cout << "\n\nTotal rows: " << total_row << endl
+         << endl;
+}
+
+int CSVParser::find_uid_index(int uid)
+{
+    // since data is stored insorted order, doing binary search.
+    int left = 0;
+    int right = parsed_data.size() - 1;
+    int mid;
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+        int mid_value = stoi(parsed_data[mid][0]);
+        if (mid_value > uid)
+        {
+            right = mid - 1;
+        }
+        else if (mid_value < uid)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+void CSVParser::find_row(int uid)
+{
+    int uid_index = find_uid_index(uid);
+
+    if (uid_index == -1)
+    {
+        cout << "Error: No record found for UID " << uid << " !!!" << endl;
+        return;
+    }
+
+    print_columns_name();
+    print_row(parsed_data[uid_index]);
+    cout << endl;
+}
+
+void CSVParser::insert_row()
+{
+    if (!validators_configured)
+    {
+        cout << "Error: Input validator functions not configured !!!" << endl;
+        exit(1);
+    }
+
+    cout << "Waiting for new record..." << endl;
+    vector<string> new_row;
+    string new_uid = to_string(prev_uid + 1);
+    new_row.push_back(new_uid);
+    for (int i = 1; i < columns_name.size(); ++i)
+    {
+        string new_cell = input_validators[columns_name[i]]();
+        new_row.push_back(new_cell);
+    }
+    parsed_data.push_back(new_row);
+    ++prev_uid;
+    set_column_width(new_row);
+    cout << "New record inserted!" << endl
+         << endl;
+}
+
+int CSVParser::update_row(int uid)
+{
+    if (!validators_configured)
+    {
+        cout << "Error: Input validator functions not configured !!!" << endl;
+        exit(1);
+    }
+
+    int uid_index = find_uid_index(uid), i = 0;
+
+    if (uid_index == -1)
+    {
+        cout << "Error: No record found for UID " << uid << "!!!" << endl;
+        return 0;
+    }
+
+    //print details for found uid.
+    print_columns_name();
+    print_row(parsed_data[uid_index]);
+    cout << endl;
+    // ask for each column to update.
+    for (int i = 1; i < columns_name.size(); ++i)
+    {
+        string option;
+        cout << "Update " << columns_name[i] << " for UID " << uid << " [Y/N]?:";
+        option = ask_YN();
+
+        if (option == "Y")
+        {
+            string newValue;
+            newValue = input_validators[columns_name[i]]();
+            parsed_data[uid_index][i] = newValue;
+        }
+    }
+    set_column_width(parsed_data[uid_index]);
+
+    cout << "UID " << uid << " updated." << endl
+         << endl;
+    return 1;
+}
+
+int CSVParser::delete_row(int uid)
+{
+    int uid_index = find_uid_index(uid);
+    if (uid_index == -1)
+    {
+        cout << "Error: No record found for UID " << uid << " !!!" << endl;
+        return 0;
+    }
+    parsed_data.erase(parsed_data.begin() + uid_index);
+
+    cout << "UID " << uid << " removed." << endl;
+    return 1;
+}
+
+void CSVParser::filter_by_column(string column_name, string value, bool ignore_case = false)
+{
+
+    int column_index = -1;
+    for (int i = 0; i < columns_name.size(); ++i)
+    {
+        if (columns_name[i] == column_name)
+        {
+            column_index = i;
+            break;
+        }
+    }
+
+    if (column_index == -1)
+    {
+        cout << "Error: Invalid column Name!!!" << endl;
+        return;
+    }
+
+    print_columns_name();
+    int total = 0, matched = 0;
+    for (auto &row : parsed_data)
+    {
+        string cell = row[column_index];
+        if (ignore_case)
+        {
+            value = to_lower(value);
+            cell = to_lower(cell);
+        }
+
+        if (cell == value)
+        {
+            print_row(row);
+            ++matched;
+        }
+        ++total;
+    }
+    cout << "\nTotal matched: " << matched << endl;
+    cout << "Total row: " << total << endl
+         << endl;
+}
+
+void CSVParser::save_modified(string new_file, bool save_column = false)
+{
+    int delim = new_file.find('.');
+    string extension = new_file.substr(delim + 1);
+    extension = to_lower(extension);
+    if (extension != "csv")
+    {
+        cout << "Warning: csv extension added !!!" << endl;
+        new_file += ".csv";
+    }
+    fstream file;
+
+    file.open(new_file, ios::out);
+
+    if (!file.good())
+    {
+        cout << "Error: Cann't create file " << new_file << " !!!" << endl;
+        return;
+    }
+
+    int last_index = parsed_data[0].size() - 1;
+
+    if (save_column)
+    {
+        for (int i = 1; i < columns_name.size(); i++)
+        {
+
+            if (i == last_index)
+            {
+                file << columns_name[i] << endl;
+            }
+            else
+            {
+                file << columns_name[i] << ",";
+            }
+        }
+    }
+
+    for (auto &row : parsed_data)
+    {
+        for (int i = 1; i < row.size(); i++)
+        {
+
+            if (i == last_index)
+            {
+                file << row[i] << endl;
+            }
+            else
+            {
+                file << row[i] << ",";
+            }
+        }
+    }
+    file.close();
+
+    cout << "Changes saved to " << new_file << endl;
+}
